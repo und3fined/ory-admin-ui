b95c17da4e4dbf0edbbe60a7ebdc8b1be934e7dd
Code Manager <code-manager@toss>
2020-08-01 15:25:06 +0700

    save for work

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
 /**
  * OryApp
  */
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+
+import defaultTheme from 'themes/chakra'
+import 'themes/global.css'
 
 export default function OryApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,6 +2,20 @@
  * About page
  */
 
+import { Box } from '@chakra-ui/core'
+
+import styles from './style.module.css'
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <Box
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <h2 className={styles.hello}>About</h2>
+    </Box>
+  )
 }
